1086e8d6484b78c9899bee0cd47b47cc0586295e
OELS <oelsart@gmail.com>
2024-07-08 19:27:08 +0900

    test用

--- Source/BlendedTerrainUtil.cs
+++ Source/BlendedTerrainUtil.cs
@@ -19,17 +19,20 @@ namespace NanameFloors
             {
                 newTerr.burnedDef = BlendInner(new TerrainMask(terrainMask.maskTextureName, baseTerrain.burnedDef ?? baseTerrain, coverTerrain.burnedDef ?? coverTerrain));
             }
+            var takenHashes = AccessTools.StaticFieldRefAccess<Dictionary<Type, HashSet<ushort>>>(typeof(ShortHashGiver), "takenHashesPerDeftype");
             DefGenerator.AddImpliedDef(newTerr);
             var bluePrintDef = (ThingDef)AccessTools.Method(typeof(ThingDefGenerator_Buildings), "NewBlueprintDef_Terrain").Invoke(typeof(ThingDefGenerator_Buildings), new object[] { newTerr, false });
+            bluePrintDef.shortHash = 0;
+            AccessTools.Method(typeof(ShortHashGiver), "GiveShortHash").Invoke(typeof(ShortHashGiver), new object[] { bluePrintDef, typeof(ThingDef), takenHashes[typeof(ThingDef)] });
             DefGenerator.AddImpliedDef(bluePrintDef);
-            newTerr.blueprintDef = bluePrintDef;
             var frameDef = (ThingDef)AccessTools.Method(typeof(ThingDefGenerator_Buildings), "NewFrameDef_Terrain").Invoke(typeof(ThingDefGenerator_Buildings), new object[] { newTerr, false });
+            frameDef.shortHash = 0;
+            AccessTools.Method(typeof(ShortHashGiver), "GiveShortHash").Invoke(typeof(ShortHashGiver), new object[] { frameDef, typeof(ThingDef), takenHashes[typeof(ThingDef)] });
             DefGenerator.AddImpliedDef(frameDef);
-            newTerr.frameDef = frameDef;
 
             LongEventHandler.ExecuteWhenFinished(delegate
             {
-                GraphicRequest req = new GraphicRequest(typeof(Graphic_Terrain), baseTerrain.texturePath, AddedShaders.TerrainHardBlend, Vector2.one, baseTerrain.DrawColor, coverTerrain.DrawColor, null, 0, null, "NanameFloors/" + terrainMask.maskTextureName);
+                GraphicRequest req = new GraphicRequest(typeof(Graphic_Terrain), baseTerrain.texturePath, AddedShaders.TerrainHardBlend, Vector2.one, baseTerrain.DrawColor, coverTerrain.DrawColor, null, 0, null, "NanameFloors/TerrainMasks/" + terrainMask.maskTextureName);
                 req.renderQueue = ((req.renderQueue == 0 && req.graphicData != null) ? req.graphicData.renderQueue : req.renderQueue);
                 newTerr.graphic = new Graphic_Terrain();
                 newTerr.graphic.Init(req);
@@ -37,7 +40,7 @@ namespace NanameFloors
                 if (!ModsConfig.BiotechActive) return;
                 Shader shader = baseTerrain.pollutionShaderType == ShaderTypeDefOf.TerrainFadeRoughLinearAdd ? AddedShaders.TerrainFadeRoughLinearAddBlend : AddedShaders.TerrainHardPollutedBlend;
                 string path = baseTerrain.pollutedTexturePath.NullOrEmpty() ? baseTerrain.texturePath : baseTerrain.pollutedTexturePath;
-                newTerr.graphicPolluted = GraphicDatabase.Get(typeof(Graphic_Terrain), path, shader, Vector2.one, baseTerrain.DrawColor, coverTerrain.DrawColor, "NanameFloors/" + terrainMask.maskTextureName);
+                newTerr.graphicPolluted = GraphicDatabase.Get(typeof(Graphic_Terrain), path, shader, Vector2.one, baseTerrain.DrawColor, coverTerrain.DrawColor, "NanameFloors/TerrainMasks/" + terrainMask.maskTextureName);
                 var matSingle = newTerr.graphicPolluted.MatSingle;
                 matSingle.SetTexture("_MainTexTwo", ContentFinder<Texture2D>.Get(coverTerrain.pollutedTexturePath ?? coverTerrain.texturePath));
                 if (!baseTerrain.pollutionOverlayTexturePath.NullOrEmpty()) matSingle.SetTexture("_BurnTex", ContentFinder<Texture2D>.Get(baseTerrain.pollutionOverlayTexturePath));
